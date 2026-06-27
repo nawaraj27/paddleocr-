503535452cef48b6a9120598403c9e7ff59f7579
nawaraj27 <159599809+nawaraj27@users.noreply.github.com>
2026-06-27 23:11:05 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,4 +34,8 @@ exec gunicorn config.wsgi:application \
     --bind "0.0.0.0:${PORT:-8000}" \
     --workers 2 \
     --timeout 120 \
+    --graceful-timeout 30 \
+    --access-logfile - \
+    --error-logfile - \
+    --log-level debug
     --graceful-timeout 30
